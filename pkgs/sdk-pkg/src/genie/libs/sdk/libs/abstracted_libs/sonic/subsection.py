d7ef6a267c48092f7a3903f4e98b941075ee0f86
jeaubin <jeaubin@cisco.com>
2019-04-16 13:55:56 -0400

    SONiC Subsection

---
+++ pkgs/sdk-pkg/src/genie/libs/sdk/libs/abstracted_libs/sonic/subsection.py
@@ -0,0 +1,50 @@
+
+# python
+import re
+import logging
+
+# pyats
+from ats import aetest
+
+log = logging.getLogger(__name__)
+
+def save_device_information(device, **kwargs):
+    """Show version to print information users interest
+
+    Args:
+      Mandatory:
+        device (`obj`) : Device object.
+
+    Returns:
+        True: Result is PASSED
+
+
+    Raises:
+        None
+
+    Example:
+        >>> save_device_information(device=Device())
+    """
+
+    # bypass the section
+    log.info('SONiC device does not have bootvar.')
+
+
+def get_default_dir(device):
+    """ Get the default directory of this device
+
+        Args:
+          Mandatory:
+            device (`obj`) : Device object.
+
+        Returns:
+            default_dir (`str`): Default directory of the system
+
+        Raises:
+            None
+
+        Example:
+            >>> get_default_dir(device=device)
+    """
+    # bypass the section
+    log.info('SONiC device does not have default directory.')
