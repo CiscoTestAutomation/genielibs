6ed196baabac998dbaf713d0291fe201e1013c12
Mehdi Cherifi <73710391+cherifimehdi@users.noreply.github.com>
2023-12-29 20:18:35 +0100

    Add files via upload
    add test_api_get_eigrp_neighbors.py

---
+++ pkgs/sdk-pkg/src/genie/libs/sdk/apis/tests/iosxe/eigrp/get/get_eigrp_neighbors/test_api_get_eigrp_neighbors.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+from pyats.topology import loader
+from genie.libs.sdk.apis.iosxe.eigrp.get import get_eigrp_neighbors
+
+
+class TestGetEigrpNeighbors(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        testbed = f"""
+        devices:
+          R1:
+            connections:
+              defaults:
+                class: unicon.Unicon
+              a:
+                command: mock_device_cli --os iosxe --mock_data_dir {os.path.dirname(__file__)}/mock_data --state connect
+                protocol: unknown
+            os: iosxe
+            platform: iosxe
+            type: iosxe
+        """
+        self.testbed = loader.load(testbed)
+        self.device = self.testbed.devices['R1']
+        self.device.connect(
+            learn_hostname=True,
+            init_config_commands=[],
+            init_exec_commands=[]
+        )
+
+    def test_get_eigrp_neighbors(self):
+        result = get_eigrp_neighbors(self.device, 'default', 1)
+        expected_output = ['192.168.1.252', '192.168.1.253']
+        self.assertEqual(result, expected_output)
+
+    def test_get_eigrp_neighbors_1(self):
+        result = get_eigrp_neighbors(self.device, 'default', 22)
+        expected_output = None
+        self.assertEqual(result, expected_output)
