ef80a65fd78360be0c67c249490a4f20696f031c
Mehdi Cherifi <73710391+cherifimehdi@users.noreply.github.com>
2024-04-01 16:46:08 +0100

    Update verify.py
    Change AS_N to auto_sys

--- pkgs/sdk-pkg/src/genie/libs/sdk/apis/iosxe/eigrp/verify.py
+++ pkgs/sdk-pkg/src/genie/libs/sdk/apis/iosxe/eigrp/verify.py
@@ -127,17 +127,17 @@ def verify_eigrp_router_id(
     device,
     router_id=None,
     vrf="default",
-    AS_N=None,
+    auto_sys=None,
     ip="ipv4",
     max_time=60,
     check_interval=10,
 ):
-    """verify EIGRP IDs for a given vrf and AS active instance for ipv4 or ipv6
+    """verify EIGRP IDs for a given vrf and auto_sys active instance for ipv4 or ipv6
     Args:
       device (obj): Device object
       router_id = None (list): List of router ID to check
       vrf = "default" (str): Name of the vrf by default set to "default"
-      AS_N = None (int) : Autonomous System
+      auto_sys = None (int) : Autonomous System
       ip = "ipv4" (str): Protocol ip set by default to "ipv4" to change to "ipv6"
       max_time (`int`): Max time, default: 30
       check_interval (`int`): Check interval, default: 10
@@ -146,14 +146,14 @@ def verify_eigrp_router_id(
       True
       False
     """
-    assert isinstance(AS_N, int), "AS_N must be int"
+    assert isinstance(auto_sys, int), "auto_sys must be int"
     assert isinstance(vrf, str), "vrf must be str"
     assert isinstance(router_id, list), "router_id must be list of router IDs"
-    assert AS_N != 0, "AS_N must not be 0"
+    assert auto_sys != 0, "auto_sys must not be 0"
     assert ip in ["ipv4", "ipv6"], "ip must be ipv4 or ipv6"
     timeout = Timeout(max_time, check_interval)
     while timeout.iterate():
-        if AS_N and router_id:
+        if auto_sys and router_id:
             try:
                 response = device.parse(
                     f"show {'ipv6' if ip!='ipv4' else 'ip'} eigrp topology"
@@ -163,18 +163,18 @@ def verify_eigrp_router_id(
                 continue
             eigrp_id = []
             if (vrf not in response.q.get_values("vrf")) or (
-                str(AS_N) not in response.q.get_values("eigrp_instance")
+                str(auto_sys) not in response.q.get_values("eigrp_instance")
             ):
                 log.error(
-                    f"Sorry, no data for the provided for 'AS = {AS_N}' and/or 'vrf = {vrf}'"
+                    f"Sorry, no data for the provided for 'auto_sys = {auto_sys}' and/or 'vrf = {vrf}'"
                 )
             else:
                 eigrp_id = (
-                    response.q.contains_key_value("eigrp_instance", str(AS_N))
+                    response.q.contains_key_value("eigrp_instance", str(auto_sys))
                     .contains(vrf)
                     .contains((ip[0:2]).upper() + ip[2:])
                     .get_values("eigrp_id")
                 )
             return set(router_id).issubset(eigrp_id)
-        log.error(f"Please, provid a valid format for AS, router_id, vrf and/or ip")
+        log.error(f"Please, provid a valid format for auto_sys, router_id, vrf and/or ip")
         continue
