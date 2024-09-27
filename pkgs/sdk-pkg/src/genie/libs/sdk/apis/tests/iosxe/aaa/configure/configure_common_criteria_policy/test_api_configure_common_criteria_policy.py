73bcdf74d6e565108fad3198835e38ff6d8f5922
Aditya Vardhan <adivardh@cisco.com>
2024-09-27 19:51:15 +0530

    Added test to the same test_api_configure_common_criteria_policy

--- pkgs/sdk-pkg/src/genie/libs/sdk/apis/tests/iosxe/aaa/configure/configure_common_criteria_policy/test_api_configure_common_criteria_policy.py
+++ pkgs/sdk-pkg/src/genie/libs/sdk/apis/tests/iosxe/aaa/configure/configure_common_criteria_policy/test_api_configure_common_criteria_policy.py
@@ -3,8 +3,37 @@ import unittest
 from pyats.topology import loader
 from genie.libs.sdk.apis.iosxe.aaa.configure import configure_common_criteria_policy
 
-
 class TestConfigureCommonCriteriaPolicy(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        testbed = f"""
+        devices:
+          Router:
+            connections:
+              defaults:
+                class: unicon.Unicon
+              a:
+                command: mock_device_cli --os iosxe --mock_data_dir {os.path.dirname(__file__)}/mock_data --state connect
+                protocol: unknown
+            os: iosxe
+            platform: vwlc
+            type: wlc
+        """
+        self.testbed = loader.load(testbed)
+        self.device = self.testbed.devices['Router']
+        self.device.connect(
+            learn_hostname=True,
+            init_config_commands=[],
+            init_exec_commands=[]
+        )
+
+    def test_configure_common_criteria_policy(self):
+        result = configure_common_criteria_policy(self.device, 'ABCD', None, None, None, None, None, None, 1, None, None, None, False, None)
+        expected_output = None
+        self.assertEqual(result, expected_output)
+
+class TestConfigureCommonCriteriaPolicyBase(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
