8d37e85efe388c56de34c2d62895855d1697f00b
Aditya Vardhan <adivardh@cisco.com>
2024-09-27 19:36:49 +0530

    Adding unit test for chamges added

--- pkgs/sdk-pkg/src/genie/libs/sdk/apis/tests/iosxe/aaa/configure/configure_common_criteria_policy/test_api_configure_common_criteria_policy.py
+++ pkgs/sdk-pkg/src/genie/libs/sdk/apis/tests/iosxe/aaa/configure/configure_common_criteria_policy/test_api_configure_common_criteria_policy.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from pyats.topology import loader
 from genie.libs.sdk.apis.iosxe.aaa.configure import configure_common_criteria_policy
@@ -7,21 +8,21 @@ class TestConfigureCommonCriteriaPolicy(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        testbed = """
+        testbed = f"""
         devices:
-          VTP-PK1:
+          Router:
             connections:
               defaults:
                 class: unicon.Unicon
               a:
-                command: mock_device_cli --os iosxe --mock_data_dir mock_data --state connect
+                command: mock_device_cli --os iosxe --mock_data_dir {os.path.dirname(__file__)}/mock_data --state connect
                 protocol: unknown
             os: iosxe
-            platform: cat9k
-            type: c9200
+            platform: vwlc
+            type: wlc
         """
         self.testbed = loader.load(testbed)
-        self.device = self.testbed.devices['VTP-PK1']
+        self.device = self.testbed.devices['Router']
         self.device.connect(
             learn_hostname=True,
             init_config_commands=[],
@@ -29,6 +30,6 @@ class TestConfigureCommonCriteriaPolicy(unittest.TestCase):
         )
 
     def test_configure_common_criteria_policy(self):
-        result = configure_common_criteria_policy(self.device, 'ABCD', 5, 0, 0, 1, 1, 20, 8, 0, 1, 5, True, 1)
+        result = configure_common_criteria_policy(self.device, 'ABCD', None, None, None, None, None, None, 1, None, None, None, False, None)
         expected_output = None
         self.assertEqual(result, expected_output)
