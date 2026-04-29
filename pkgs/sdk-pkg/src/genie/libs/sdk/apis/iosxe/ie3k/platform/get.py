ae25b6ad96af19752438a53ea31bc6248af96034
Thomas Ryan <thomryan@cisco.com>
2026-04-29 09:30:09 -0400

    Restore IE3K get_config_register API

--- pkgs/sdk-pkg/src/genie/libs/sdk/apis/iosxe/ie3k/platform/get.py
+++ pkgs/sdk-pkg/src/genie/libs/sdk/apis/iosxe/ie3k/platform/get.py
@@ -5,6 +5,9 @@ import re
 # Genie
 from genie.metaparser.util.exceptions import SchemaEmptyParserError
 
+# Unicon
+from unicon.core.errors import SubCommandFailure
+
 # Logger
 log = logging.getLogger(__name__)
 
@@ -46,3 +49,42 @@ def get_boot_variables(device, boot_var, output=None):
                 boot_images.append(item)
 
     return boot_images
+
+
+def get_config_register(device, next_reload=False, output=None):
+    """Get current config-register setting on the device.
+
+    Args:
+        device (`obj`): Device object
+        next_reload (`bool`): Determine if returning next-reload value (unused)
+        output (`str`): Command output
+
+    Returns:
+        config-register value or None
+    """
+
+    if output is None:
+        if device.state_machine.current_state == 'rommon':
+            try:
+                output = device.execute("set")
+            except SubCommandFailure as e:
+                log.error(
+                    "Failed to execute 'set' command on device in rommon state\n{}".
+                    format(str(e))
+                )
+                return None
+        else:
+            try:
+                output = device.execute("show romvar")
+            except SubCommandFailure as e:
+                log.error(
+                    "Failed to execute 'show romvar' command on device\n{}".
+                    format(str(e))
+                )
+                return None
+
+    match = re.search(r"ConfigReg\s*=\s*(?P<confreg>\w+)", output)
+    if not match:
+        log.error("Failed to parse config register value: {}".format(output))
+        return None
+    return match.group("confreg")
